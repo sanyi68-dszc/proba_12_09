97336758511d01ebd797fb0d1b6cf20684c7be4e
Sajat Nev <email@pelda.hu>
2025-12-09 14:43:40 +0100

    JS menü

---
+++ alap.js
@@ -0,0 +1,23 @@
+const menuSor=`<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+<div class="container-fluid">
+  <a class="navbar-brand" href="#">Logo</a>
+  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav">
+      <li class="nav-item">
+        <a class="nav-link" href="index.html">Nyitólap</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="menu1.html">Menü 1.</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="menu2.html">Menü 2.</a>
+      </li>
+    </ul>
+  </div>
+</div>
+</nav>`
+
+document.getElementById("menuSor").innerHTML=menuSor
