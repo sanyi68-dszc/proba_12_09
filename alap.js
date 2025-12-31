1e1c315cde410772f5f7eeb2b6aeff583653d3dd
0Mart00 <nyiri.martin.101@gmail.com>
2025-12-31 23:50:55 +0100

    Update

--- alap.js
+++ alap.js
@@ -1,6 +1,6 @@
 const menuSor=`<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
 <div class="container-fluid">
-  <a class="navbar-brand" href="#">Logo</a>
+  <a class="navbar-brand" href="#">alMA Gang</a>
   <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
     <span class="navbar-toggler-icon"></span>
   </button>
